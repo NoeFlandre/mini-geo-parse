ee3d25a20623e3d55aa289ebf0bcc0cb471dc94b
NoeFlandre <noeflandre@gmail.com>
2025-12-29 21:35:47 +0530

    Built the entire prompt for the LLM

--- src/llm_resolver.py
+++ src/llm_resolver.py
@@ -12,8 +12,8 @@ OLLAMA_URL = "http://localhost:11434/api/generate"
 MODEL = "ministral-3:3b"
 
 def resolve_location(
-    #original_text : str,
-    #entity : str,
+    original_text : str,
+    entity : str,
     candidates : list[dict]
 ) -> dict | None:
 
@@ -43,12 +43,26 @@ def resolve_location(
         for i, c in enumerate(candidates)
     )
 
-    return options_text
+    prompt = f"""Given the following text :
+    "{original_text}"
+
+    The entity "{entity}" could refer to one of these locations :
+    "{options_text}"
+
+    Which location is most likely being referenced in the text?
+    Reply with ONLY the number (1, 2, 3, etc) corresponding to the correct option.
+
+    """
+
+    return prompt
 
 if __name__ == "__main__":
     paris_candidates = [
         {"name": "Paris, Île-de-France, France", "lat": 48.8534951, "lon": 2.3483915},
         {"name": "Paris, Lamar County, Texas, USA", "lat": 33.6617962, "lon": -95.555513},
     ]
-    options_text = resolve_location(paris_candidates)
-    print(options_text)
+
+    paris_original_text = "I went to Paris to see the Eiffel Tower."
+    entity = "Paris"
+    output = resolve_location(paris_original_text, entity, paris_candidates)
+    print(output)
